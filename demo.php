33fce7c7605ccc245bcf44599e1d7bdc75d77521
phpfdocx <64880836+phpfdocx@users.noreply.github.com>
2024-05-20 21:29:35 -0300

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,64 +1,70 @@
 <?php
 /*
- * PhpFDocx 1.0.0
+ * PhpFDocx 2.0.0
  *
  * @link https://github.com/phpfdocx/phpfdocx
- * @author Humberto Fornazier
+ * @author Humberto Fornazier - Brazil
  * contact: phpfdocx@gmail.com
  * @since 05.03.2020 
 */
 
-/* 
-    ************************* VERY IMPORTANT: ***********************
-	1) Variables to be searched for and replaced must be in lowercase
-	
-    Do not use numbers in the variables to be replaced.
-    Example: ${phone3}, ${name1}, {$email4) ...
-
-	Variables cannot have spaces, symbols or special characters. 	
-	Example:
-	my home = must be myhome
-	my_home = must be myhome
- 	
-
+/*
 */
 include 'PhpFDocx.php';
+//$doc  = 'test1.docx';
+//$doc  = 'test2.docx';
+//$doc  = 'test3.docx';
+//$doc  = 'test4.docx';
+//$doc  = 'test5.docx';
+//$doc  = 'test6.docx';
 
-$doc          = 'test1.docx';
-//$doc        = 'test2.docx';
-//$doc        = 'test3.docx';
-//$doc        = 'test4.docx';
-//$doc        = 'test5.docx';
-//$doc        = 'test6.docx';
+$doc = 'test1.docx';
 
-$aDataSearch = Array (  'name'                    ,
-						'address'                 ,
-						'city'                    ,
-						'region'                  ,
-						'country'                 ,
-						'zip'                     ,
-						'personalnumber'          ,
-						'organizationnumber'      ,				   
-						'email'                   ,				   
-						'today'                   ,
-                        'header'                  ,						
-                        'titledocumentchange'     );
+$aDataSearch = Array (  
+                        'name'                    ,					
+			'NAME'                    ,
+			'address'                 ,
+			'ADDRESS'                 ,
+			'city'                    ,
+			'CITY'                    ,						
+			'region'                  ,
+			'REGION'                  ,						
+			'country'                 ,
+			'COUNTRY'                 ,						
+			'zip'                     ,
+			'personalnumber'          ,
+			'personal-number'         ,						
+			'organizationnumber'      ,				   
+			'email'                   ,				   
+			'today'                   , 				
+                        'documenttitle'           ,
+			'[test$variable]'         ,				
+			'test_complex!and@long-variable|for#tests'					
+		      );
 
-$aDataChange = Array (  'Greyce, MacKensie X.'    ,
-						'Ap #537-6485 Morbi Road' , 
-						'Saint-Georges'           , 
-						'Konya'                   , 
-						'Austria'                 ,
-						'741488'                  ,
-						'16911214 2808'           ,
-						'16911468 1404'           ,				   
-						'placerat.orci@quam.edu4' ,
-						date('Y/m/d')             ,	
-						'New header Changed'      ,						
-						'Document title changed'  );
+$aDataChange = Array (  
+                        'Greyce, MacKensie X.'      ,
+                        'GREYCE, MACKENSIE X.'      ,						
+			'Ap #288-1785-B5 Morbi Road', 
+			'AP #288-1785-B5 MORBI ROAD',
+			'Saint-Georges'             , 
+			'SAINT-GEORGES'             , 						
+			'Konya'                     , 
+			'KONYA'                     ,						
+			'Austria'                   ,
+			'AUSTRIA'                   ,						
+			'741488'                    ,
+			'16911214 2808'             ,						
+			'16911214 2808'             ,							
+			'16911468 1404'             ,				   
+			'placerat.orci@quam.edu'    ,
+			date('Y/m/d')               ,				
+			'Document Title Changed'    ,
+			'Teste Variable Changed'    ,
+			'test variable complex and long changed'					
+		      );				
 						
 $result = PhpFDocx( $doc , $aDataSearch , $aDataChange );	
 
-echo '<a href="'.$result.'">View Generated Document = <b>'.$result.'</b></a><br /><br />';	
-	
+echo '<a href="'.$result.'">View Generated Document = <b>'.$result.'</b></a><br /><br />';		
 ?>				   
